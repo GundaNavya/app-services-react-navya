c29b9edbcada49fd50316bbbbb4b7c43ab6addb2
Pavankumar Vangala <pavankumar.vangala@kony.com>
2018-11-28 14:19:45 +0530

    asdc

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -6,46 +6,86 @@ var itemStatus = {
         items: [
           {
             item: "Cloud Environment Identification	",
-            status: "Passsed"            
+            status: "Passsed",
+            // options : {
+            //   modal : "Enfield",modal1 : "Yamaha", modal2 : "Bajaj"
+            // },
+            options: [
+              {"modal" : "Enfield"},
+              {"modal" : "Yamaha"},
+              {"modal" : "Bajaj"}              
+            ]            
           },
           {
             item: "Access to Reporting Queue",
-            status: "Passsed"            
+            status: "Passsed" ,
+            options: [
+              {"modal" : "Enfield"},
+              {"modal" : "Yamaha"},
+              {"modal" : "Bajaj"}              
+            ]           
           },
           {
             item: "Security Credentials	",
-            status: "Failed"             
+            status: "Failed" ,
+            options: [
+              {"modal" : "Enfield"},
+              {"modal" : "Yamaha"},
+              {"modal" : "Bajaj"}              
+            ]             
           },
           {
             item: "Static Resources Location",
-            status: "Passsed"               
+            status: "Passsed",
+            options: [
+              {"modal" : "Enfield"},
+              {"modal" : "Yamaha"},
+              {"modal" : "Bajaj"}              
+            ]                 
           },
           {
             item: "Access to Deployment Storage	",
-            status: "Passsed"            
+            status: "Passsed",
+            options: [
+              {"modal" : "Enfield"},
+              {"modal" : "Yamaha"},
+              {"modal" : "Bajaj"}              
+            ]            
           },
           {
             item: "Access to Device DB",
-            status: "Passsed"              
+            status: "Passsed" ,
+           options: [
+              {"modal" : "Enfield"},
+              {"modal" : "Yamaha"},
+              {"modal" : "Bajaj"}              
+            ]               
           },
           {
             item: "Access to Cache",
-            status: "Failed"              
+            status: "Failed" ,
+            options: [
+              {"modal" : "Enfield"},
+              {"modal" : "Yamaha"},
+              {"modal" : "Bajaj"}              
+            ]            
           }
         ]
      }
   };
+  
 
   var ExpanseDashboardPage = React.createClass({
     render: function() {
       //To retrieve keys from data.content
       var keys = Object.keys(itemStatus.content);
       console.log(keys)
+      console.log(itemStatus.content)
    
       //iterate through the keys to get the underlying values
-      var allEmps = ['items'].map((t) => itemStatus.content[t].map((e) => (<tbody><tr><td>{e.item}</td><td className={ e.status === "Failed" ? "failed" : "success"}>{e.status}</td></tr></tbody>)) );
+      var allEmps = ['items'].map((t) => itemStatus.content[t].map((e) => (<tbody><tr><td>{e.item}</td><td className={ e.status === "Failed" ? "failed" : "success"}>{e.status}</td><td><select>{e.options.map((k) => <option>{k.modal}</option>)}</select></td></tr></tbody>)) );
    
-      return (<table className="table-border" width="100%"><thead><th>Item</th><th>Status</th></thead>{allEmps}</table>)
+      return (<table className="table-border" width="100%"><thead><tr><th>Item</th><th>Status</th><th></th></tr></thead>{allEmps}</table>)
     }
   });
 
--- src/components/SideNavbar.js
+++ src/components/SideNavbar.js
@@ -7,7 +7,8 @@ import {Link, NavLink} from 'react-router-dom';
 var SideHeader = React.createClass({
     getInitialState: function(){
         return {
-            hover: false
+            hover: false,
+            hoverIcon: false
         };
     },
     hoverOn: function(e){
@@ -15,6 +16,12 @@ var SideHeader = React.createClass({
     },
     hoverOff: function(e){
         this.setState({ hover: false });
+    },
+    hoverOnIcon: function(e){
+        this.setState({ hoverIcon: true });
+    },
+    hoverOffIcon: function(e){
+        this.setState({ hoverIcon: false });
     },
     render: function(){
         return (
@@ -28,15 +35,15 @@ var SideHeader = React.createClass({
             <li><NavLink to="/" className="kony-logo"><img className={ this.state.hover ? "hover-logo" : "main-logo"} 
             onMouseEnter={this.hoverOn} 
             onMouseLeave={this.hoverOff} src="/../../images/svg_icons/logo.svg"/></NavLink></li>
-            <li><NavLink to="/apps" className="icons apps selected" activeClassName="is-active" exact={true}> <img className="icon32" src="/../../images/svg_icons/icon-apps-inactive.svg"/><span className="ml-10">Apps</span></NavLink></li>            
-            <li><NavLink to="/gettext" className="icons dashboard" activeClassName="is-active" exact={true} ><img className="icon32" src="/../../images/svg_icons/icon-edit-inactive.svg"/><span className="ml-10">Monitoring </span></NavLink></li>
-            <li><NavLink to="/jobs" className="icons api" activeClassName="is-active"  exact={true}> <img className="icon32" src="/../../images/svg_icons/icon-edit-inactive.svg"/><span className="ml-10">Jobs</span></NavLink></li>
+            <li><NavLink to="/apps" className="icons apps selected" activeClassName="is-active" exact={true}> <img className={ this.state.hoverIcon ? "icon-edit" : "icon-edit-inactive"} onMouseEnter={this.hoverOnIcon} onMouseLeave={this.hoverOffIcon} /><span className="ml-10 module">Apps</span></NavLink></li>            
+            <li><NavLink to="/gettext" className="icons dashboard" activeClassName="is-active" exact={true} ><img className="icon32" src="/../../images/svg_icons/icon-edit-inactive.svg"/><span className="ml-10 module">Monitoring </span></NavLink></li>
+            <li><NavLink to="/jobs" className="icons api" activeClassName="is-active"  exact={true}> <img className="icon32" src="/../../images/svg_icons/icon-edit-inactive.svg"/><span className="ml-10 module">Jobs</span></NavLink></li>
             
-            <li><NavLink to="/healthcheck" className="icons dev-portal" activeClassName="is-active" exact={true}> <img className="icon32" src="/../../images/svg_icons/icon-edit-inactive.svg"/><span className="ml-10">Health Check</span></NavLink></li>
-            <li><NavLink to="/contact" className="icons clouds" activeClassName="is-active" exact={true}><img className="icon32" src="/../../images/svg_icons/icon-edit-inactive.svg"/><span className="ml-10">Logs </span> </NavLink></li>
+            <li><NavLink to="/healthcheck" className="icons dev-portal" activeClassName="is-active" exact={true}> <img className="icon32" src="/../../images/svg_icons/icon-edit-inactive.svg"/><span className="ml-10 module">Health Check</span></NavLink></li>
+            <li><NavLink to="/contact" className="icons clouds" activeClassName="is-active" exact={true}><img className="icon32" src="/../../images/svg_icons/icon-edit-inactive.svg"/><span className="ml-10 module">Logs </span> </NavLink></li>
             
-            <li><NavLink to="/about" className="icons settings" activeClassName="is-active" exact={true}><img className="icon32" src="/../../images/svg_icons/icon-settings-inactive.svg"/><span className="ml-10">Settings</span> </NavLink></li>
-            <li><NavLink to="/career" className="icons support" activeClassName="is-active" exact={true}> <img className="icon32" src="/../../images/svg_icons/icon-reports-inactive.svg"/><span className="ml-10">Reoprts</span></NavLink></li>
+            <li><NavLink to="/about" className="icons settings" activeClassName="is-active" exact={true}><img className="icon32" src="/../../images/svg_icons/icon-settings-inactive.svg"/><span className="ml-10 module">Settings</span> </NavLink></li>
+            <li><NavLink to="/career" className="icons support" activeClassName="is-active" exact={true}> <img className="icon32" src="/../../images/svg_icons/icon-reports-inactive.svg"/><span className="ml-10 module">Reoprts</span></NavLink></li>
             <li className="product-name"><a className={ this.state.hover ? "hover-product" : "main-product"} 
             onMouseEnter={this.hoverOn} 
             onMouseLeave={this.hoverOff} >Kony Fabric Console </a></li></ul> </div>
