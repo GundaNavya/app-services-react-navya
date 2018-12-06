0a4c204b3a796ce4b5a53679edd5c56e5e79628f
Lakshminavya Gunda <lakshminavya.gunda@kony.com>
2018-12-06 16:28:16 +0530

    change in css file

--- src/components/ResponseOp.js
+++ src/components/ResponseOp.js
@@ -52,7 +52,7 @@ class IntResOp extends Component{
                         <div id="collapseTwo" className="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
                             <div className="panel-body">
                            
-                          <div className="col-sm-12 pr-0">
+                          <div className="col-sm-12 pr-0 pl-0">
                                 <div>
                                     <h4>Header</h4>
                                     <table className="table-border col-sm-12 mt-10">
@@ -68,7 +68,7 @@ class IntResOp extends Component{
                                 </div>
                            
                           </div>
-                          <div className="col-sm-12 pr-0">
+                          <div className="col-sm-12 pr-0 pl-0">
                                 <h4 className="mt-20">Body</h4>
                                 <div className="mt-10"> Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.</div>
                           </div>
